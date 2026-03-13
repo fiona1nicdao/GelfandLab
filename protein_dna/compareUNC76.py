f0a6a07dbd2ccb2a64e4728a68ab72d866e5d812
FionaN <fiona1941nicdao@gmail.com>
2026-03-13 09:18:57 -0500

    sort files to protein dna contructs

---
+++ protein_dna/compareUNC76.py
@@ -0,0 +1,64 @@
+# original SjGST-UNC76-myc vs gst-unc76trunc-myc
+"""
+INFO:: coordinates transformed by orthogonal matrix: 
+|         1, 1.845e-16,-6.324e-17|
+| 2.175e-16,         1, 3.398e-16|
+|-7.757e-17,-3.664e-16,         1|
+( 1.008e-14, -4.08e-15,-4.114e-15)
+  Rotation - polar (omega,phi,kappa)  0.0000 0.0000 0.0000
+  Rotation - euler (alpha,beta,gamma) 0.0000 0.0000 -0.0000
+  Translation - Angstroms             0.0000 -0.0000 -0.0000 
+INFO: core rmsd achieved: 1.0884 Angstroems
+      number of residues in reference structure: 729
+      number of residues in moving structure:    450
+      number of residues in aligned sections (reference):  729
+      number of residues in aligned sections (moving):     450
+      number of aligned residues:  227
+      number of gaps:              2
+      number of misdirections:     0
+      number of SSE combinations:  877184.0000
+      sequence identity:           97.3568%
+"""
+
+## original SjGST-UNC76-myc vs gst-unc76DELETED-myc
+"""
+INFO:: coordinates transformed by orthogonal matrix: 
+|         1, 8.073e-17,-2.943e-17|
+|  1.56e-16,         1, 3.614e-17|
+|-1.942e-16,-2.851e-16,         1|
+( 6.536e-15, -1.14e-14, 2.275e-15)
+  Rotation - polar (omega,phi,kappa)  0.0000 0.0000 0.0000
+  Rotation - euler (alpha,beta,gamma) 0.0000 0.0000 -0.0000
+  Translation - Angstroms             0.0000 -0.0000 0.0000 
+INFO: core rmsd achieved: 3.6458 Angstroems
+      number of residues in reference structure: 729
+      number of residues in moving structure:    668
+      number of residues in aligned sections (reference):  729
+      number of residues in aligned sections (moving):     668
+      number of aligned residues:  231
+      number of gaps:              2
+      number of misdirections:     0
+      number of SSE combinations:  41119425.0000
+      sequence identity:           96.1039%
+"""
+# gst-unc76trunc-myc  vs gst-unc76DELETED-myc
+"""
+INFO:: coordinates transformed by orthogonal matrix: 
+|         1, -0.001344,  0.005759|
+|  0.001966,     0.994,   -0.1094|
+| -0.005577,    0.1094,     0.994|
+(   -0.2157,    0.8738,     1.448)
+  Rotation - polar (omega,phi,kappa)  89.1341 2.9669 6.2876
+  Rotation - euler (alpha,beta,gamma) -86.9855 6.2869 87.0807
+  Translation - Angstroms             -0.2157 0.8738 1.4482 
+INFO: core rmsd achieved: 2.4008 Angstroems
+      number of residues in reference structure: 450
+      number of residues in moving structure:    668
+      number of residues in aligned sections (reference):  450
+      number of residues in aligned sections (moving):     668
+      number of aligned residues:  256
+      number of gaps:              6
+      number of misdirections:     0
+      number of SSE combinations:  1028755.0000
+      sequence identity:           91.7969%
+"""
